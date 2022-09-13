21bd4da0a991073c50ffb641e338c9ebcb707cf0
rodrigo-br <rodrigoab123@gmail.com>
2022-09-13 23:23:39 +0200

    pegando path mas não usando ainda

--- src/main.c
+++ src/main.c
@@ -6,14 +6,49 @@
 /*   By: ralves-b <ralves-b@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/12 17:11:39 by wportilh          #+#    #+#             */
-/*   Updated: 2022/09/13 19:07:56 by ralves-b         ###   ########.fr       */
+/*   Updated: 2022/09/13 22:03:17 by ralves-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
 
-int	main(int argc, char **argv)
+static char	**pick_path(char **envp, int i)
 {
+	char	**temp;
+	char	**paths;
+
+	temp = NULL;
+	while (envp[i])
+	{
+		if (ft_strnstr(envp[i], "PATH=", 5))
+		{
+			temp = ft_split(envp[i] + 5, ':');
+			break ;
+		}
+		i++;
+	}
+	if (temp == NULL)
+		ft_putstr_fd("PATH not found", 1);
+	i = 0;
+	while (temp[i])
+		i++;
+	paths = (char **)malloc((i + 1) * sizeof(*temp));
+	paths[i] = NULL;
+	while (i--)
+	{
+		paths[i] = ft_strjoin(temp[i], "/");
+		free(temp[i]);
+	}
+	free(temp);
+	return (paths);
+}
+
+int	main(int argc, char **argv, char **envp)
+{
+	char	**paths;
+
+	paths = pick_path(envp, 0);
+	(void)paths;
 	if (argv && argc > 1)
 	{
 		ft_putstr_fd("Error. No arguments are necessary", STDOUT_FILENO);

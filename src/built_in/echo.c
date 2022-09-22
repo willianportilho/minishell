07836e8e1355d42f1816ac117b823ac4f80f6b27
wportilh <willianportilho01@gmail.com>
2022-09-22 05:34:57 +0200

    fixed echo

--- src/built_in/echo.c
+++ src/built_in/echo.c
@@ -6,7 +6,7 @@
 /*   By: wportilh <wportilh@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/22 00:08:52 by wportilh          #+#    #+#             */
-/*   Updated: 2022/09/22 00:46:35 by wportilh         ###   ########.fr       */
+/*   Updated: 2022/09/22 05:30:36 by wportilh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,20 +17,19 @@ void	echo(t_table **tab)
 	int	i;
 
 	i = 0;
-	if ((ft_str_is_equal((*tab)->cmd_line[i++], "echo")) && (*tab)->cmd_line[i])
+	if ((ft_str_is_equal((*tab)->cmd_line[i], "echo")) && (*tab)->cmd_line[++i])
 	{
 		if (ft_str_is_equal((*tab)->cmd_line[i], "-n"))
-		{
 			i++;
-			while ((*tab)->cmd_line[i])
-				ft_printf("%s ", (*tab)->cmd_line[i++]);
-		}
-		else
+		while ((*tab)->cmd_line[i])
 		{
-			while ((*tab)->cmd_line[i])
+			if ((*tab)->cmd_line[i] && (!(*tab)->cmd_line[i + 1]))
+				ft_printf("%s", (*tab)->cmd_line[i++]);
+			else
 				ft_printf("%s ", (*tab)->cmd_line[i++]);
-			ft_printf("\n");
 		}
+		if (!(ft_str_is_equal((*tab)->cmd_line[1], "-n")))
+			ft_printf("\n");
 	}
 	else
 		ft_printf("\n");

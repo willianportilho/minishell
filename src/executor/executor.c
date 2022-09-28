57bbe433dcb88545e1cd1c3039c2b285339257dc
rodrigo-br <rodrigoab123@gmail.com>
2022-09-28 02:39:49 +0200

    pescando problema na next_n_free_tab

--- src/executor/executor.c
+++ src/executor/executor.c
@@ -6,7 +6,7 @@
 /*   By: ralves-b <ralves-b@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/19 18:47:12 by wportilh          #+#    #+#             */
-/*   Updated: 2022/09/27 17:05:10 by ralves-b         ###   ########.fr       */
+/*   Updated: 2022/09/28 02:39:24 by ralves-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -106,10 +106,10 @@ void	executor(t_table **tab)
 		{
 			initialize_files(&aux);
 			initialize_childs(&aux, &exec);
-			if (exec.i > 0)
-				next_n_free_tab(aux);
-			else
-				aux = aux->next;
+			// if (exec.i > 0)
+			// 	next_n_free_tab(aux);
+			// else
+			aux = aux->next;
 		}
 		close_pipes(&exec);
 		wait_processes(&exec);

279d95da08596679f4aa74c9d1a790c52a4db674
rodrigo-br <rodrigoab123@gmail.com>
2022-09-28 02:35:18 +0200

    retornado historico

--- src/executor/exec_clean.c
+++ src/executor/exec_clean.c
@@ -6,7 +6,7 @@
 /*   By: ralves-b <ralves-b@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/20 22:39:09 by wportilh          #+#    #+#             */
-/*   Updated: 2022/09/27 17:05:20 by ralves-b         ###   ########.fr       */
+/*   Updated: 2022/09/28 02:35:03 by ralves-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 
 void	clean_alloc(t_exec *exec)
 {
-	rl_clear_history();
 	ft_free_int_array(&exec->pipes);
 	ft_free_vector(&exec->pid);
 }

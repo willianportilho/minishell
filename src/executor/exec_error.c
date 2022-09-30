9a43f77410df9a5bc90556d9bd671aa32cbc5ccd
wportilh <willianportilho01@gmail.com>
2022-09-30 03:53:10 +0200

    Adicionada mensagem de erro personalizada quando PATH não está presente

--- src/executor/exec_error.c
+++ src/executor/exec_error.c
@@ -6,7 +6,7 @@
 /*   By: wportilh <wportilh@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/22 18:43:44 by wportilh          #+#    #+#             */
-/*   Updated: 2022/09/22 18:59:24 by wportilh         ###   ########.fr       */
+/*   Updated: 2022/09/30 03:51:02 by wportilh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,16 @@ void	cmd_error(t_table **tab, t_exec *exec)
 {
 	char	*str;
 	char	*str2;
+	char	*path;
 
-	str = ft_strjoin((*tab)->cmd_line[0], ": command not found\n");
+	path = simple_expander(ft_strdup("PATH"));
+	if (ft_strlen(path))
+		str = ft_strjoin((*tab)->cmd_line[0], ": command not found\n");
+	else
+		str = ft_strjoin((*tab)->cmd_line[0], ": No such file or directory\n");
 	str2 = ft_strjoin("minishell: ", str);
 	ft_putstr_fd(str2, 2);
+	free(path);
 	free(str);
 	free(str2);
 	clean_alloc(exec);

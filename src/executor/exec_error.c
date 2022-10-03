4a3d9d498ad6f31ae6761b67b0bde28c399faf32
rodrigo-br <rodrigoab123@gmail.com>
2022-10-03 23:36:58 +0200

    resolvido /src

--- src/executor/exec_error.c
+++ src/executor/exec_error.c
@@ -6,7 +6,7 @@
 /*   By: ralves-b <ralves-b@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/22 18:43:44 by wportilh          #+#    #+#             */
-/*   Updated: 2022/09/30 18:25:58 by ralves-b         ###   ########.fr       */
+/*   Updated: 2022/10/03 23:33:27 by ralves-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,8 @@ void	cmd_error(t_table **tab, t_exec *exec)
 	if (!stat((*tab)->cmd_line[0], &sb) && ((sb.st_mode & S_IFMT) == S_IFDIR)
 		&& ft_strchr((*tab)->cmd_line[0], '/'))
 		global()->exit = 126;
+	else if ((*tab)->cmd_line[0][0] == '/')
+		global()->exit = 127;
 	else
 	{
 		path = simple_expander(ft_strdup("PATH"));
--- src/executor/executor.c
+++ src/executor/executor.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   executor.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: wportilh <wportilh@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: ralves-b <ralves-b@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/19 18:47:12 by wportilh          #+#    #+#             */
-/*   Updated: 2022/10/03 20:01:52 by wportilh         ###   ########.fr       */
+/*   Updated: 2022/10/03 23:36:47 by ralves-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,23 @@ static void	validate_path(t_table **tab, t_exec *exec)
 
 	if (!stat((*tab)->cmd_line[0], &sb) && ((sb.st_mode & S_IFMT) == S_IFDIR)
 		&& ft_strchr((*tab)->cmd_line[0], '/'))
-	{
+	{//criar função pra essa msg, recebendo char * cmd_line[0]
 		ft_putstr_fd("minishell: ", 2);
 		ft_putstr_fd((*tab)->cmd_line[0], 2);
 		ft_putstr_fd(": Is a directory\n", 2);
 		exec->pos = -1;
 		return ;
 	}
+	if ((*tab)->cmd_line[0][0] == '/')
+	{//chamar a função acima aqui
+		ft_putstr_fd("minishell: ", 2);
+		ft_putstr_fd((*tab)->cmd_line[0], 2);
+		ft_putstr_fd(": No such file or directory\n", 2);
+		exec->pos = -1;
+		return ;
+	}
+	ft_putstr_fd((*tab)->cmd_line[0], 2);
+	ft_putstr_fd("\n", 2);
 	exec->pos = -1;
 	exec->check = TRUE;
 	while ((*tab)->path[++exec->pos])
